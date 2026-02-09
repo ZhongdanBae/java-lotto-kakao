c0b4a65bedb32c08b12cefadab68cfa32d9725ba
tjrwns1021@gmail.com <tjrwns1021@gmail.com>
2026-02-09 17:50:17 +0900

    Test: Vendor 천원 단위의 금액이 들어오지 않았을 때
    천원단위로 입력해달라는 에러를 Throw

--- src/main/java/model/Vendor.java
+++ src/main/java/model/Vendor.java
@@ -3,6 +3,8 @@ package model;
 public class Vendor {
 
     public Lottos sell(int money){
+        if (money%1000 > 0) throw new RuntimeException("천원 단위의 금액을 입력해주세요.");
+
         return new Lottos();
     }
 }
--- src/test/java/TestVendor.java
+++ src/test/java/TestVendor.java
@@ -1,16 +1,29 @@
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import model.Vendor;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TestVendor {
     @Test
     void 올바른_금액이_주어졌을때는_적정_개수의_로또를_반환한다(){
-        int money = 13700;
+        Integer money = 13000;
 
         Vendor vendor = new Vendor();
 
         assertThat(vendor.sell(money).getQuantity()).isEqualTo(13);
 
+    }
+
+    @Test
+    void 천원_단위의_금액이_들어오지_않았을_때_에러를_반환한다() {
+        Integer money = 13700;
+        Vendor vendor = new Vendor();
+        try {
+            vendor.sell(money);
+            fail();
+        }
+        catch (RuntimeException e) {
+            assertThat(e.getMessage()).isEqualTo("천원 단위의 금액을 입력해주세요.");
+        }
     }
 }
